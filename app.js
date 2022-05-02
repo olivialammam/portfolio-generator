4476f1be03ea3c93be8775b8bcf19894467c638f
Olivia Lammam <olivia.l7475@gmail.com>
2022-05-01 19:07:57 -0700

    quesions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
+const generatePage = require('./project/src/page-template');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -129,11 +129,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-     const pageHTML = generatePage();
+    const pageHTML = generatePage(portfolioData);
 
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
 
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
